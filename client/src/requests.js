8283e0b28b336a5a772e96ec46fc4dece5369821
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-05-05 12:02:57 +0430

    Fetch a job using its id from server and show it in the JobDetail component.

--- client/src/requests.js
+++ client/src/requests.js
@@ -22,3 +22,28 @@ export async function loadJobs() {
   const body = await response.json();
   return body.data.jobs;
 }
+
+export async function loadJob(id) {
+  const response = await fetch(endpointURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      query: `
+        query singleJob($id: ID!) {
+          job(id: $id) {
+            id
+            title
+            description
+            company {
+              id
+              name
+            }
+          }
+        }
+      `,
+      variables: { id },
+    }),
+  });
+  const body = await response.json();
+  return body.data.job;
+}
